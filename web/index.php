3893b6c4d75d4ab803cfebcd54669a3c8950675a
Mat Carey <mat@matcarey.co.uk>
2014-04-26 16:19:26 +0100

    Basic styling

--- web/index.php
+++ web/index.php
@@ -1,2 +1,15 @@
 <!DOCTYPE html>
-<h1>This website has not been created ... yet.</h1>
+<style>
+body {
+  background: black;
+  color: white;
+  font-family: sans-serif;
+  text-align: center;
+  font-weight: normal;
+}
+a {
+  color: inherit;
+}
+</style>
+<a href="http://dorightdigital.com/"><img src="logo.jpg" alt="Do Right Digital"/></a>
+<h1>This website has not been created yet, it's being held by Do Right Digital.</h1>
